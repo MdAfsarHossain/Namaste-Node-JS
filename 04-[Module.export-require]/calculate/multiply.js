705f036bda9fddc60c7cf0cde4e22ba8e472ad59
MdAfsarHossain <afsar291722@gmail.com>
2025-10-20 15:41:25 +0600

    Multiply js file code updated

--- 04-[Module.export-require]/calculate/multiply.js
+++ 04-[Module.export-require]/calculate/multiply.js
@@ -1,6 +1,6 @@
 function calculateMultiply(a, b) {
   let result = a * b;
-  console.log(result);
+  console.log(`${a} and ${b} multiplication is: ${result}`);
 }
 
 module.exports = { calculateMultiply };
