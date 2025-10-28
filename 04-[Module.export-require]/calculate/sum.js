0017b7f334b17de932960e6e966172f345e62d12
MdAfsarHossain <afsar291722@gmail.com>
2025-10-28 08:02:19 +0600

    Updated file added

--- 04-[Module.export-require]/calculate/sum.js
+++ 04-[Module.export-require]/calculate/sum.js
@@ -6,7 +6,7 @@ function calculateSum(a, b) {
   console.log(sum);
 }
 
-console.log(module.exports); // {} It's means
+console.log(module.exports); // {} It's means empty
 
 module.exports = {
   x,
